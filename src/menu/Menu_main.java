57917fca0a03953a8e8c0c0d2eb5c280efaf489a
sho <sho@macbook-air>
2018-01-15 17:43:40 +0900

    50616041

---
+++ src/menu/Menu_main.java
@@ -0,0 +1,12 @@
+package menu;
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Menu_view frame = new Menu_view();
+		frame.setBounds(5,5,1000,800);
+		frame.setVisible(true);
+	}
+
+}
